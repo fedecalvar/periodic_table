adcd9ab66371791b651d82eba8121afe742bb265
Federico Calvar <calvarfede01@gmail.com>
2024-11-09 16:27:22 +0000

    fix: correct SQL query to handle element argument properly

--- element.sh
+++ element.sh
@@ -6,7 +6,15 @@ if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 else
-  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1 OR symbol = '$1' OR name = '$1'")
+  # Determinar si el argumento es un número (número atómico) o un texto (símbolo o nombre)
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1")
+  else
+    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1'")
+  fi
+
+  # Verificar si se encontró el elemento
   if [[ -z $ELEMENT ]]
   then
     echo "I could not find that element in the database."
